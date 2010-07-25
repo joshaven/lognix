2d8ede52e32def5bf1506fc3cc92dd1dc62d9e3b
Joshaven Potter <yourtech@gmail.com>
2010-07-24 20:24:19 -0400

    Displaying a page

--- logviewer.js
+++ logviewer.js
@@ -1,45 +1,35 @@
-// // Variables
-// var http = require('http'),
-//     fs = require('fs'),
-//     file =  '/var/log/install.log';
-// 
-// // Requires
-// 
-// 
-// http.createServer(function (request, responce) {
-//   responce.writeHead(200, {'Content-Type': 'text/plain'});
-//   responce.end(fs.readFileSync(file));
-// }).listen(8124, "127.0.0.1");
-// 
-// console.log('Server running at http://127.0.0.1:8124/');
-// 
-// 
-
-
 var express = require('express'),
     connect = require('connect'),
     fs      = require('fs'),
     file    = '/var/log/install.log',
     app     = express.createServer();
 
+// Default URL
 app.get('/', function(req, res){
-  res.header(200, {'Content-Type': 'text/plain'});
-  
-  // Need to chunk the file to the escapedText function... and maybe chunck the responce?
-  res.send('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml"><head><meta content="text/html;charset=utf-8" http-equiv="Content-Type" /><title>Log Viewer</title></head><body>' 
-    // + escapedText( fs.readFileSync(file) )
-    + fs.readFileSync(file)
-    + '</body></html>');
-  
-  // res.send(fs.readFileSync(file));
+  console.log('Redirecting to: /public/index.html');
+  res.redirect('/public/index.html');  
+});
+
+// Respond to any files in the public folder
+app.get('/public/*.*', function(req, res, params){
+  var req_type = res.contentType('.'+req.url);
+  console.log(' --> '+req.method+'(HTTP/'+req.httpVersion+') "'+req.url+'" <'+req_type+'>');
+  res.send(fs.readFileSync('.'+req.url, 'utf8'));
 });
 
 app.listen(3000);
 
 console.log('Server running at http://127.0.0.1:3000');
-
-function escapedText( str ) {
-  return str.replace( 
-    /[<&>'"#]/g, 
-    function(s) { return {'<':'&lt;', '>':'&gt;', '&':'&amp;', '"':'&quot;', "'":'&#039;', '#':'&#035;' }[s]; });
-}
+// 
+// 
+// 
+// function escapedText( str ) {
+//   return str.replace( 
+//     /[<&>'"#]/g, 
+//     function(s) { return {'<':'&lt;', '>':'&gt;', '&':'&amp;', '"':'&quot;', "'":'&#039;', '#':'&#035;' }[s]; });
+// }
+// 
+// // The following may be faster, this should be checked.
+// // function escapedText(mystring){
+// //   return mystring.replace(/&/g, "&amp;").replace(/#/:'&#035;').replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;").replace(/'/g:'&#039;');
+// //   };
